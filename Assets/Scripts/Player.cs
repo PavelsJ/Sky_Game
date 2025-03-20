49db09819344a2a9063e2ad5c236c607cd57f3e2
arsen <76913401+PavelsJ@users.noreply.github.com>
2025-03-20 12:10:56 +0200

    G_V3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,75 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float force = 10;
-    private Rigidbody rb; 
+    public float force = 20;
+    public float acceleration = 200;
+
+    public float turnSpeed = 60, minAcceleration, maxAcceleration, minSpeed = 0, maxSpeed = 200;
+    
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    
+    private bool isGrounded;
+    
+    private Rigidbody rb;
+    private Animator animator;
+    
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.Linecast(transform.position, groundCheck.position, groundLayer);
         
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            TurnLeft();
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            TurnRight();
+        }
     }
     
     void FixedUpdate()
     {
+        if (isGrounded)
+        {
+            float angle = Mathf.Abs(180 - transform.eulerAngles.y);
+            acceleration = Remap(0, 90, maxAcceleration, minAcceleration, angle);
+            
+            force += acceleration * Time.fixedDeltaTime;
+            force = Mathf.Clamp(acceleration, minSpeed, maxSpeed);
+            
+            animator.SetFloat("playerSpeed", force);
+            
+            Vector3 forceVector = transform.forward * (force * Time.fixedDeltaTime);
+            forceVector.y = rb.velocity.y;
         
+            rb.velocity = forceVector;
+        }
+    }
+
+    private void TurnRight()
+    {
+        if (transform.eulerAngles.y > 91)
+        {
+            transform.Rotate(new Vector3(0, -turnSpeed * Time.deltaTime, 0), Space.Self);
+        }
+    }
+
+    private void TurnLeft()
+    {
+        if (transform.eulerAngles.y < 269)
+        {
+            transform.Rotate(new Vector3(0, turnSpeed * Time.deltaTime, 0), Space.Self);
+        }
+    }
+    
+    private float Remap (float value, float from1, float to1, float from2, float to2) {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
 }
